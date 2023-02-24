88a2250581370751e61a14f832ab464c6818a94b
Mariano Silva <mariano.silva@dtidigital.com>
2023-02-24 10:17:39 -0300

    banner ok

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Banner.css"
+const Banner = () => {
+  return (
+    <header className='banner'>
+       <img src="/images/banner.png" alt="O banner principal da página da Organo" />
+    </header>
+   
+  )
+}
+
+export default Banner
